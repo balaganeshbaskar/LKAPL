3d952777aa2505bf337842df586fd2e673aba149
balaganeshbaskar <balaganeshbaskar@gmail.com>
2023-11-03 20:17:29 +0530

    Add files via upload

---
+++ js/scripts.js
@@ -0,0 +1,21 @@
+/*!
+* Start Bootstrap - Modern Business v5.0.7 (https://startbootstrap.com/template-overviews/modern-business)
+* Copyright 2013-2023 Start Bootstrap
+* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-modern-business/blob/master/LICENSE)
+*/
+// This file is intentionally blank
+// Use this file to add JavaScript to your project
+
+// function tab_choice(product)
+// {
+
+//     const queryString = window.location.search;
+//     console.log(queryString);
+//     const urlParams = new URLSearchParams(queryString);
+//     const tabname = urlParams.get('company')
+//     console.log(tabname);
+//     // #v-pills-gn
+
+//     var someTabTriggerEl = document.getElementById('v-pills-it');
+//     someTabTriggerEl.show();
+// }
